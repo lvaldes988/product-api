71665f94a95bb1c082270a26858fd55660b5cba7
liset valdes <lvaldes@techlaunch.io>
2018-04-22 08:32:08 -0400

    categories and products done

---
+++ api/categoties/read_one_category.php
@@ -0,0 +1,15 @@
+<?php
+// include core configuration
+include_once('../../config/core.php');
+// include database connection
+include_once('../../config/database.php');
+// category object
+include_once('../../objects/category.php');
+// class instance
+$database = new Database();
+$db = $database->getConnection();
+$category = new Category($db);
+// read all the categories
+$results = $category->readAll();
+// output in json format
+echo $results;
